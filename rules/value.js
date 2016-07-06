65188bfaf1bd4ab2b1f09b5a4fa1f057c510c5cb
Jonathan Zempel <jzempel@gmail.com>
2016-07-06 15:42:03 -0700

    [#5] Ignore unicode-range values with uppercase "U+" prefix

--- rules/value.js
+++ rules/value.js
@@ -3,7 +3,7 @@
 module.exports = {
   'rules': {
     // Specify lowercase or uppercase for keywords values
-    'value-keyword-case': 'lower',
+    'value-keyword-case': ['lower', { 'ignoreKeywords': ['/^U\+.+$/'] }],
     // Disallow vendor prefixes for values
     'value-no-vendor-prefix': true
   }
