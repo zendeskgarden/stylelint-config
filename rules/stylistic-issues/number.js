13ffbf1820b7949564090b1b7d5ba30ea1f5076e
Jonathan Zempel <jzempel@gmail.com>
2018-03-15 16:23:06 +0800

    Add copyright header

--- rules/stylistic-issues/number.js
+++ rules/stylistic-issues/number.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
 module.exports = {
   rules: {
     // Require or disallow a leading zero for fractional numbers less than 1
