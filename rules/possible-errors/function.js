bd01e5f1524df744e129c3f41d309c80313b9f24
Jonathan Zempel <jzempel@gmail.com>
2019-01-23 09:23:00 -0800

    Add `function-calc-no-invalid` rule

--- rules/possible-errors/function.js
+++ rules/possible-errors/function.js
@@ -7,6 +7,8 @@
 
 module.exports = {
   rules: {
+    // Disallow an invalid expression within `calc` functions
+    'function-calc-no-invalid': true,
     // Disallow an unspaced operator within `calc` functions
     'function-calc-no-unspaced-operator': true,
     // Disallow direction values in `linear-gradient()` calls that are not valid according to the standard syntax
